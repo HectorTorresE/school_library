42d586e5f4101fd99b993b6e427e4fbc3d6db387
HectorTorresE <hectorjte@gmail.com>
2022-07-19 11:08:30 -0600

    Add decorators

--- person.rb
+++ person.rb
@@ -1,10 +1,13 @@
-class Person
+require './nameable'
+
+class Person < Nameable
   # Constructor
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    super
   end
 
   attr_reader :id
@@ -13,6 +16,10 @@ class Person
 
   def can_use_services?
     @age >= 18 || parent_permission
+  end
+
+  def correct_name
+    name
   end
 
   private
