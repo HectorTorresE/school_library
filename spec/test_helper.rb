64d28d5aa07cd48e4d8a7a225514f9d35d428f02
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-07-27 11:18:23 -0500

    Create test_helper functions

---
+++ spec/test_helper.rb
@@ -0,0 +1,68 @@
+def title
+  'THE SUN ALSO RISES'
+end
+
+def author
+  'ERNEST HEMINGWAY'
+end
+
+def id
+  123
+end
+
+def age
+  23
+end
+
+def name
+  'ROBERTO BANOS'
+end
+
+def label
+  '101'
+end
+
+def create_classroom
+  Classroom.new(label)
+end
+
+def create_student(classroom = create_classroom)
+  Student.new(age, classroom, name, id)
+end
+
+def create_book
+  Book.new(title, author)
+end
+
+def book_json
+  "{\"json_class\":\"Book\",\"data\":[\"#{title}\",\"#{author}\"]}"
+end
+
+def classroom_json
+  "{\"json_class\":\"Classroom\",\"data\":[\"#{label}\"]}"
+end
+
+def create_person
+  Person.new(age, name, id)
+end
+
+def person_json
+  "{\"json_class\":\"Person\",\"data\":[#{age},\"#{name}\",#{id},true]}"
+end
+
+def specialization
+  'Mathematics'
+end
+
+def student_json
+  "{\"json_class\":\"Student\",\"data\":[#{age},{\"json_class\":\"Classroom\",\
+\"data\":[\"101\"]},\"#{name}\",#{id},true]}"
+end
+
+def create_teacher
+  Teacher.new(age, specialization, name, id)
+end
+
+def teacher_json
+  "{\"json_class\":\"Teacher\",\"data\":[#{age},\"#{specialization}\",\"#{name}\",#{id}]}"
+end
